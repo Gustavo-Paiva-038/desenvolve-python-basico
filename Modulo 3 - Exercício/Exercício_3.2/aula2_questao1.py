f850594fa6533d6f3aa6badacd3d057351601473
Gustavo-Paiva-038 <gustavo.paiva@pdbomdespacho.com.br>
2024-11-08 18:07:57 -0300

    Delete Modulo 3 - Exercício/Exercício_3.2 directory

--- Modulo 3 - Exercício/Exercício_3.2/aula2_questao1.py
+++
@@ -1,9 +0,0 @@
-# 1 - 
-# Juliana e Cris querem entrar em um bar, mas só podem se ambos forem maiores de idade (>17).
-# Escreva um programa que solicite as idades de Juliana e Cris e imprima True se ambas forem maiores de 17 anos,
-# indicando que podem entrar no bar, e False caso contrário.
-
-idad_ju=int(input(f"Informe a idade de Juliana: "))
-idad_cris = int(input(f"Informe a idade de Cris: "))
-
-print (idad_ju>17 and idad_cris>17)
